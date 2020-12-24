88e094bd3632425897a9b1c21da6706e8f0bfe94
Akankshap38 <akankshapardeshi03@gmail.com>
2020-12-24 17:05:29 +0530

    Option to add favourite movie in the list added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,19 +3,18 @@ import Header from "./Header"
 import SearchBox from "./SearchBox"
 import MovieCard from "./MovieCards"
 import { Row} from 'react-bootstrap';
+
 function App(){
 
     const [movieList, updateMovieList] = useState([]);
     const [searchValue, setSearchValue ] = useState('');
+    const [favList, updateFavList] = useState([]);
 
     async function searchChanged(searchValue){
         console.log(searchValue+" sent from SearchBox components...");
         const response = await fetch('http://www.omdbapi.com/?s='+searchValue+'&apikey=5c3f7439');
         const responseJson = await response.json();
-        console.log(responseJson);
-        console.log(responseJson.Search);
         console.log(Array.isArray(responseJson.Search));
-
         if(responseJson.Search&&responseJson.Search.length>0)
             updateMovieList(responseJson.Search)
     }
@@ -26,19 +25,30 @@ function App(){
             searchChanged(searchValue);
     },[searchValue]);
 
+
+    function addFavouriteToList(title){
+        updateFavList(prevValue=>{
+            return [...prevValue, title] ;
+        })
+    }
+
+    useEffect(()=>{
+        console.log(favList);
+    },[favList]);
+    
     return (<div className='container-fluid movie-app'>
         <div className='row d-flex align-items-center mt-4 mb-4'>
             <Header />
             <SearchBox onSearchChange={setSearchValue}/>
             <Row>
                 { movieList.map((element,index)=>{
-                    return (<MovieCard imgLink={element.Poster} title={element.Title} key={index}/>)
+                    return (<MovieCard imgLink={element.Poster} title={element.Title} key={index} onFavClick={addFavouriteToList}/>)
                     })
                 } 
             </Row>
         </div>
     </div>)
-
 }
+
 
 export default App;
--- src/Components/MovieCards.jsx
+++ src/Components/MovieCards.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Col } from 'react-bootstrap';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
 function MovieCard(props){
     return (<Col className="image-container d-flex justify-content-start">
         <img src={props.imgLink} alt={props.title}/>
+        <div className="overlay" onClick={()=>{
+            props.onFavClick(props.title);
+        }} >
+         Add to your Favourite
+        <span>
+            <FavoriteIcon style={{ color: "red" }}></FavoriteIcon>
+        </span>
+        </div>
     </Col>)
 }
 
-export default MovieCard
+export default MovieCard;
